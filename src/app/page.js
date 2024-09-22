e36fe42efbe79205e190130ad48787342ba2b8d5
Vatinius <102.23.dylan.chan@gmail.com>
2024-09-22 17:38:25 +0800

    Revert to stable

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import About from "./about/about";
 
 export default function Home() {
   return (
-    
+    <h1>Hello world</h1>
   );
 }
